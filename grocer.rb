f5f8460a69e647a04929a66da5ca05931b7edcfe
isabel k. lee <hello@isabelklee.co>
2020-02-27 20:02:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,34 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  index = 0
+  while index < collection.length do 
+    if collection[index][:item] == name 
+      return collection[index]
+    end 
+    index += 1
+  end 
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart = []
+  index = 0 
+
+  while index < cart.length do 
+    new_item = find_item_by_name_in_collection(cart[index][:item], new_cart)
+    if new_item != nil 
+      new_item[:count] += 1
+    else 
+      new_item = {
+        :item => cart[index][:item],
+        :price => cart[index][:price],
+        :clearance => cart[index][:clearance],
+        :count => 1
+      }
+      new_cart.push(new_item)
+    end 
+    index += 1
+  end 
+
+  return new_cart
 end
 
 def apply_coupons(cart, coupons)
